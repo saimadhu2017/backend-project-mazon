c31603d3b4b7de5ea32c62d7daf6ffc731f81824
sai madhu k <34480223+saimadhu2017@users.noreply.github.com>
2022-10-16 21:40:23 +0530

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('../app');
 
 const server = http.createServer(app);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 server.listen(process.env.PORT, () => {
     console.log('server is up and running');
