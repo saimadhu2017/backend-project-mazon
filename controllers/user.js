3180f20668a72c28bfb5a301a29f10662a2fc61d
saimadhu2017 <madhusai2017@gmail.com>
2022-10-30 21:07:06 +0530

    added new route for signing up a user

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { buildPassword } = require('../helpers/password');
 const executeAPI = require('../database/exFunction');
 const { ok, badReq } = require('../helpers/httpcodes');
+const validators = require('../helpers/validators');
 
 const onDone = (rows, res) => {
     res.status(ok.code).json({
@@ -14,15 +15,22 @@ const onError = (err, res) => {
     res.status(badReq.code).json({
         status: badReq.status,
         message: badReq.message,
-        err: err
+        err: err.message || err[0].message
     })
 }
 
 exports.setUser = (req, res) => {
-    const user = {
-        ...req.body,
-        ...buildPassword(req.body.password)
+    const valid = validators.validateUser(req.body);
+    if (valid.error) {
+        onError(valid.error.details, res)
     }
-    const sql = 'select * from [users].[customers]';
-    executeAPI(sql, onDone, onError, res)
+    else {
+        const user = {
+            ...req.body,
+            ...buildPassword(req.body.password)
+        }
+        const sql = `EXEC users.sp_signup_user '${user.first_name}','${user.last_name}',
+        '${user.mail}','${user.phone}','${user.password}','${user.salt}'`;
+        executeAPI(sql, onDone, onError, res);
+    }
 }
---
+++ helpers/validators.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+const nameMaxLength = 30;
+const phoneLength = 10;
+const phonePattren = /^\d{10}$/
+const minAge = 18;
+const dobPattern = /^\d{4}-\d{2}-\d{2}$/;
+
+exports.validateUser = (data) => {
+    const userSchema = Joi.object({
+        first_name: Joi.string().max(nameMaxLength).required().trim(),
+        last_name: Joi.string().max(nameMaxLength).required().trim(),
+        mail: Joi.string().email().required(),
+        phone: Joi.string().length(phoneLength).required().pattern(new RegExp(phonePattren)),
+        password: Joi.string().pattern(new RegExp(passwordPattern)).required()
+    })
+    return (userSchema.validate(data));
+}
