5f3f07a6407d228e126a1b6f5cc0efc0f9009580
saimadhu2017 <madhusai2017@gmail.com>
2023-01-14 19:53:54 +0530

    done with new api call for checking user signed in

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../controllers/auth')
+const { userIdParam } = require('../controllers/user')
+
+router.param('userid', userIdParam)
 
 router.post('/signup', auth.signUp)
 router.get('/signin', auth.signIn)
 router.get('/signout', auth.signOut)
+
+router.get('/is-signed-in/:userid', auth.isSignedIn, auth.isAuthenticated, auth.userLoginValidation)
 
 module.exports = router;
